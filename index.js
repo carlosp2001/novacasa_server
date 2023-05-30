21c0850ea5258d31a32c2997262545166c1faa38
Carlos Pineda <cara_p@unicah.edu>
2023-05-30 16:05:41 -0600

    Nuevos cambios

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const server = http.createServer((req, res) => {
     } 
 });
 
-server.listen(3000, () => {
-    console.log('Listening to requests on port 3000');
+server.listen(8080, () => {
+    console.log('Listening to requests on port 8080');
 });
 
 
