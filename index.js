b28c9d60984a76bb7a67dd4a842d4dd2fec37f59
Carlos Pineda <cara_p@unicah.edu>
2023-05-30 21:50:54 -0600

    SDfafa

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type': 'text/html',
         });
-        console.log(query);
+        console.log(query.id);
 
         res.end('<h1>Hola</h1>');
 
