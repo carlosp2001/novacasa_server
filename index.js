b0a409046fd81e8958e0342e1b2659892d486760
Carlos Pineda <cara_p@unicah.edu>
2023-05-31 19:29:14 -0600

    Cambios

--- index.js
+++ index.js
@@ -39,7 +39,78 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type': 'text/html',
         });
-        console.log(query);
+
+        console.log(query.id);
+        if (query.id) {
+            // const res = await superagent.get(
+            //     `https://test.novacasahn.com/wp-json/wp/v2/properties/${query.id}`
+            //     );
+
+            con.query(
+                `SELECT t.* FROM test_novacasahn_com.wp_d3d8n7_posts t where t.post_type = 'property' and post_status='publish' and t.ID=${query.id} LIMIT 1`,
+                function (err, result, fields) {
+                    if (err) throw err;
+                    let resultArray = Array.from(result);
+                    let html = resultArray[0].post_content;
+
+                    con.query(
+                        `SELECT t.* FROM test_novacasahn_com.wp_d3d8n7_postmeta t where t.post_id = ${query.id} LIMIT 501`,
+                        async (err, result) => {
+                            let resultMetaArray = Array.from(result);
+                            let currencyMeta = resultMetaArray.find(
+                                (el) => el.meta_key == 'fave_currency'
+                            );
+                            let priceMeta = resultMetaArray.find(
+                                (el) => el.meta_key == 'fave_property_price'
+                            );
+                            const [img, ...altImgs] = await getImages(
+                                Array.from(
+                                    resultMetaArray.filter(
+                                        (el) =>
+                                            el.meta_key ==
+                                            'fave_property_images'
+                                    )
+                                )
+                            );
+
+                            let name = resultArray[0].post_title;
+                            let image_url = img;
+                            let availability = 'in stock';
+                            let retailer_id = resultArray[0].ID;
+                            let url = resultArray[0].guid;
+                            let brand = 'Novacasa';
+                            let currency = currencyMeta.meta_value;
+                            let description = html.replace(/<[^>]+>/g, '');
+                            let price = priceMeta.meta_value;
+                            let additional_image_urls = altImgs;
+
+                            superagent
+                                .post(
+                                    'https://graph.facebook.com/v17.0/198639315962847/products'
+                                )
+                                .query({
+                                    access_token:
+                                        'EAAJWZBDFSBdgBAOIAuEZA6o9779ZA6NnCFk4JFQZCjMIbT5ow6JDXNkx7B7UWtRlg0jUe3So65o8vdn8Ocs68Vp3oXZCf1tfsYMZCl2Wla9LAK6pcpmmQaZCjdQGTCLj5YVMmWLDX05ZAe2y04NBKhVQVwfAMlNJavfcPo2BGZCoYTwZDZD',
+                                })
+                                .send({
+                                    name,
+                                    image_url,
+                                    availability,
+                                    retailer_id,
+                                    url,
+                                    brand,
+                                    currency,
+                                    description,
+                                    price,
+                                    additional_image_urls,
+                                }) // sends a JSON post body
+                                .set('X-API-Key', 'foobar')
+                                .set('accept', 'json')
+                                .end((err, res) => {
+                                    // Calling the end function will send the request
+                                    if (err) console.log(err);
+                                    console.log(res);
+                                });
 
                             res.end('<h1>Hola</h1>');
                         }
